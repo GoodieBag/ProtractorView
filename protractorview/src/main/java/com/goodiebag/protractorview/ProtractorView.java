8b9b032b1969ead16bc6b5957cca6cf58ad9ec4b
Krishanu <krishanudey99@gmail.com>
2017-01-10 15:52:28 +0530

    Initial Commit

---
+++ protractorview/src/main/java/com/goodiebag/protractorview/ProtractorView.java
@@ -0,0 +1,23 @@
+package com.goodiebag.protractorview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Krishanu on 10/01/17.
+ */
+
+public class ProtractorView extends View {
+    public ProtractorView(Context context) {
+        super(context);
+    }
+
+    public ProtractorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProtractorView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
